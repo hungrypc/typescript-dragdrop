a13720bbbf12c037f2814d6d9f5fedd3fb91f126
hpc <philipchanhk626@gmail.com>
2020-06-10 09:51:50 -0400

    autobind decorator instead of manual bind or arrow function, tedious and stupid

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+// autobind decorator
+function autobind(
+  _: any, 
+  _2: string, 
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -21,7 +40,8 @@ class ProjectInput {
     this.attach()
   }
 
-  private handleSubmit = (event: Event) => {
+  @autobind
+  private handleSubmit (event: Event) {
     event.preventDefault()
     console.log(this.titleInputElement.value)
   }
